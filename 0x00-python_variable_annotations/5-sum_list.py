3fadacf32e599b7f1bc3456cf4726a4c2355a249
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-08-15 22:52:17 +0200

    [Update] - Advanced Variable annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains sum_list function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Adds list of floats"""
+    return sum(input_list)
