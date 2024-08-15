06c123adf4c4f202eb9706ca6d97080c8ccd85eb
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-08-15 22:12:44 +0200

    [Update] - Add function"

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+"""Module that contains a function add"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two variables
+
+    a (float) - first variable
+    b (float) - second variable
+    """
+    return a + b
